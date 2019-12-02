bfda872e685f4f3429499730bff03c210de28268
rayquazaW <rayquaza.wyh@gmail.com>
2019-12-02 21:13:23 +0800

    page91, 程序清单4.12

---
+++ chapter4/assgn_st.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main(){
+    using namespace std;
+    inflatable bouquet = {
+        "sunflowers",
+        0.20,
+        12.49
+    };
+    inflatable choice;
+    cout << "bouquet: " << bouquet.name << " for $" << bouquet.price << endl;
+    choice = bouquet;
+    cout << "choice: " << choice.name << " for $" << choice.price << endl;
+    cin.get();
+    return 0;
+}
